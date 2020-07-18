ae3b890b61beec6fb2717ebe5b3f410ffd54d424
RohiniBojja <58306043+RohiniBojja@users.noreply.github.com>
2020-07-18 19:06:26 +0530

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -1,3 +1,4 @@
+
 import aiohttp
 import asyncio
 import uvicorn
@@ -30,9 +31,18 @@ async def download_file(url, dest):
 
 
 async def setup_learner():
-    await download_file(export_file_url, path/'models'/f'{export_file_name}.pkl')
-    learn = load_learner(path/'models', f'{export_file_name}.pkl', device='cpu')
-    return learn
+    await download_file(export_file_url, path / export_file_name)
+    try:
+        learn = load_learner(path, export_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
 
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
